90711ce299707558c3cd38b37bfdaa2e26a38503
Gabriela Surita <gabsurita@gmail.com>
2020-10-31 15:21:05 -0300

    download ibge data

---
+++ download.sh
@@ -0,0 +1,10 @@
+# Download IBGE census data.
+wget -nc ftp://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/SP_20171016.zip
+wget -nc ftp://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/MG_20171016.zip
+wget -nc ftp://ftp.ibge.gov.br/Censos/Censo_Demografico_2010/Resultados_do_Universo/Agregados_por_Setores_Censitarios/RJ_20171016.zip
+
+# Unzip.
+unzip -o SP_20171016.zip
+unzip -o MG_20171016.zip
+unzip -o RJ_20171016.zip
+

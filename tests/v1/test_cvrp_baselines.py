3572522c46c035f561dcbf59da8a8034b0faa5fd
Fillipe Goulart <fillipe.mendes@loggi.com>
2021-02-01 23:00:03 -0300

    Add code for the LKH solver

--- tests/v1/test_cvrp_baselines.py
+++ tests/v1/test_cvrp_baselines.py
@@ -5,6 +5,7 @@ from dacite import from_dict
 
 from loggibud.v1.types import CVRPInstance
 from loggibud.v1.baselines.shared import ortools
+from loggibud.v1.baselines.task1 import lkh_3
 from loggibud.v1.eval.task1 import evaluate_solution
 
 
@@ -18,5 +19,11 @@ def toy_cvrp_instance():
 
 def test_delivery_instance_generation(toy_cvrp_instance):
     result = ortools.solve(toy_cvrp_instance)
+
+    evaluate_solution(toy_cvrp_instance, result)
+
+
+def test_lkh_solver(toy_cvrp_instance):
+    result = lkh_3.solve(toy_cvrp_instance)
 
     evaluate_solution(toy_cvrp_instance, result)

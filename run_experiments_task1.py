f9856ec4fc4c7764bc34808457f290e8224db932
Fillipe Goulart <fillipe.mendes@loggi.com>
2021-04-01 17:23:13 -0300

    Add parameters for task 1 evaluation

--- run_experiments_task1.py
+++ run_experiments_task1.py
@@ -1,6 +1,7 @@
 import json
 from dacite import from_dict
 from pathlib import Path
+from timeit import default_timer
 
 import pandas as pd
 
@@ -10,10 +11,13 @@ from loggibud.v1.eval.task1 import evaluate_solution
 from loggibud.v1.types import CVRPInstance
 
 
+# Number of repetitions for each solver in the same instance
+NUM_REPLICATIONS = 1
+
+
 if __name__ == "__main__":
-    file_instances = Path("data/cvrp-instances-1.0/").rglob("*.json")
+    file_instances = Path("data/cvrp-instances-1.0/dev/").rglob("*.json")
     solvers = {"ortools": ortools, "lkh-3": lkh_3}
-    num_replications = 1
 
     results_data = []
     for k, file_instance in enumerate(file_instances):
@@ -22,14 +26,19 @@ if __name__ == "__main__":
 
         instance = from_dict(CVRPInstance, data_file)
         for solver_name, solver in solvers.items():
-            for i in range(num_replications):
+            for i in range(NUM_REPLICATIONS):
                 print(f"Solver {solver_name} in instance {instance.name}")
+                tic = default_timer()
                 result = solver.solve(instance)
-                total_distance = (
+                total_time = default_timer() - tic
+
+                total_distance, is_feasible = (
                     evaluate_solution(instance, result)
-                    if result else -1
+                    if result else (-1, False)
                 )
-                feasible_solution = True if result else False
+
+                if not is_feasible:
+                    total_distance = -1
 
                 results_data.append({
                     "instance_name": instance.name,
@@ -37,12 +46,13 @@ if __name__ == "__main__":
                     "solver": solver_name,
                     "replication": i + 1,
                     "total_distance": total_distance,
-                    "feasible_solution": feasible_solution,
+                    "total_time": total_time,
+                    "feasible_solution": is_feasible,
                 })
 
-        # Stop early here, this must be run in another computer
-        # if k >= 30:
-            # break
+        # Print results so far
+        df_results = pd.DataFrame(results_data)
+        df_results.to_csv("task1_results_temp.csv", index=False)
 
     df_results = pd.DataFrame(results_data)
     df_results.to_csv("task1_results.csv", index=False)

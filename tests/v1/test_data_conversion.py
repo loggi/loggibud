15a8a5d8371acbc1bd23eb0a6b7db3ed7a4ca02b
Fillipe Goulart <fillipe.mendes@loggi.com>
2021-04-11 11:49:56 -0300

    Add great circle distance to all tests

--- tests/v1/test_data_conversion.py
+++ tests/v1/test_data_conversion.py
@@ -2,8 +2,10 @@ import json
 
 import pytest
 from dacite import from_dict
+from mock import patch
 
 from loggibud.v1 import data_conversion
+from loggibud.v1.distances import calculate_distance_matrix_great_circle_m
 from loggibud.v1.types import CVRPInstance
 
 
@@ -15,7 +17,20 @@ def toy_cvrp_instance():
     return from_dict(CVRPInstance, data)
 
 
-def test_can_create_proper_tsplib_from_instance(toy_cvrp_instance):
+@pytest.fixture
+def mocked_osrm_distance_matrix():
+    """Monkey-patch the OSRM distance matrix with the Great Circle"""
+
+    with patch(
+        "loggibud.v1.data_conversion.calculate_distance_matrix_m",
+        new=calculate_distance_matrix_great_circle_m,
+    ) as mock_osrm:
+        yield mock_osrm
+
+
+def test_can_create_proper_tsplib_from_instance(
+    toy_cvrp_instance, mocked_osrm_distance_matrix
+):
     tspfile = data_conversion.to_tsplib(toy_cvrp_instance)
 
     assert toy_cvrp_instance.name in tspfile
--- tests/v1/test_instance_generation.py
+++ tests/v1/test_instance_generation.py
@@ -1,12 +1,11 @@
 import pytest
+from mock import patch
 
-from loggibud.v1.types import Point
+from loggibud.v1.distances import calculate_distance_matrix_great_circle_m
 from loggibud.v1.instance_generation.generators import (
     DeliveryGenerationConfig,
     CVRPGenerationConfig,
-    prepare_census_data,
     generate_census_instances,
-    generate_deliveries,
     generate_cvrp_subinstances,
 )
 
@@ -14,6 +13,17 @@ from loggibud.v1.instance_generation.generators import (
 @pytest.fixture(params=["rj", "df", "pa"])
 def instance_name(request):
     return request.param
+
+
+@pytest.fixture
+def mocked_osrm_distance_matrix():
+    """Monkey-patch the OSRM distance matrix with the Great Circle"""
+
+    with patch(
+        "loggibud.v1.instance_generation.generators.calculate_distance_matrix_m",
+        new=calculate_distance_matrix_great_circle_m,
+    ) as mock_osrm:
+        yield mock_osrm
 
 
 def test_delivery_instance_generation(instance_name):
@@ -38,7 +48,9 @@ def test_delivery_instance_generation(instance_name):
     assert result.deliveries
 
 
-def test_cvrp_subinstance_generation(instance_name):
+def test_cvrp_subinstance_generation(
+    instance_name, mocked_osrm_distance_matrix
+):
     config = DeliveryGenerationConfig(
         name=instance_name,
         num_train_instances=3,

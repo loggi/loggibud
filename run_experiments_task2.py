553e3c7e1bfc5bbd970398c0f2e842c04d3b0d96
Fillipe Goulart <fillipe.mendes@loggi.com>
2021-05-17 14:07:26 -0300

    Use nearest neighbor on TSP solution in Task 2

--- run_experiments_task2.py
+++ run_experiments_task2.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from timeit import default_timer
 
 import pandas as pd
+from ortools.constraint_solver import routing_enums_pb2
 
 from loggibud.v1.baselines.task2 import kmeans_greedy, qrp_sweep
 from loggibud.v1.eval.task1 import evaluate_solution
@@ -12,6 +13,22 @@ if __name__ == "__main__":
     train_paths = Path("data/cvrp-instances-1.0/train/")
     dev_paths = Path("data/cvrp-instances-1.0/dev/")
     solvers = {"kmeans_greedy": kmeans_greedy, "qrp_sweep": qrp_sweep}
+    params_dict = {
+        "kmeans_greedy": kmeans_greedy.QRPModel(
+            ortools_tsp_params=kmeans_greedy.ORToolsParams(
+                first_solution_strategy=routing_enums_pb2.
+                FirstSolutionStrategy.PATH_CHEAPEST_ARC,
+                local_search_metaheuristic=None,
+            )
+        ),
+        "qrp_sweep": qrp_sweep.QRPModel(
+            ortools_tsp_params=qrp_sweep.ORToolsParams(
+                first_solution_strategy=routing_enums_pb2.
+                FirstSolutionStrategy.PATH_CHEAPEST_ARC,
+                local_search_metaheuristic=None,
+            )
+        )
+    }
 
     results_data = []
     # Iterate at the same time paths "train/df-0" and "dev/df-0", for example
@@ -29,31 +46,45 @@ if __name__ == "__main__":
         ]
         for solver_name, solver in solvers.items():
             # Trains a solver with all test instances in the `train_path`
-            model = solver.pretrain(train_instances)
+            params = params_dict[solver_name]
+            model = solver.pretrain(train_instances, params=params)
 
             # Solve each dev instance with this model
             for dev_instance in dev_instances:
                 print(f"Solver {solver_name} in instance {dev_instance.name}")
-                tic = default_timer()
-                result = solver.solve_instance(model, dev_instance)
-                total_time = default_timer() - tic
+                try:
+                    tic = default_timer()
+                    result = solver.solve_instance(model, dev_instance)
+                    total_time = default_timer() - tic
 
-                total_distance, is_feasible = (
-                    evaluate_solution(dev_instance, result)
-                    if result else (-1, False)
-                )
+                    total_distance, is_feasible = (
+                        evaluate_solution(dev_instance, result)
+                        if result else (-1, False)
+                    )
 
-                if not is_feasible:
-                    total_distance = -1
+                    if not is_feasible:
+                        total_distance = -1
 
-                results_data.append({
-                    "instance_name": dev_instance.name,
-                    "instance_size": len(dev_instance.deliveries),
-                    "solver": solver_name,
-                    "total_distance": total_distance,
-                    "total_time": total_time,
-                    "feasible_solution": is_feasible,
-                })
+                    results_data.append({
+                        "instance_name": dev_instance.name,
+                        "instance_size": len(dev_instance.deliveries),
+                        "solver": solver_name,
+                        "total_distance": total_distance,
+                        "total_time": total_time,
+                        "num_vehicles": len(result.vehicles) if result else -1,
+                        "feasible_solution": is_feasible,
+                    })
+                except TypeError:
+                    print(f"Instance {dev_instance.name} has issues so skipped")
+                    results_data.append({
+                        "instance_name": dev_instance.name,
+                        "instance_size": len(dev_instance.deliveries),
+                        "solver": solver_name,
+                        "total_distance": None,
+                        "total_time": None,
+                        "num_vehicles": -2,
+                        "feasible_solution": -2,
+                    })
 
                 # Print results so far
                 df_results = pd.DataFrame(results_data)

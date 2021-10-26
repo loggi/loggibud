eb44aadfd02aaefcd13c028e048f3378a41af357
Fillipe Goulart <fillipe.mendes@loggi.com>
2021-10-26 19:10:24 -0300

    Add support for TSPLIB95 competition conversion

--- loggibud/v1/data_conversion.py
+++ loggibud/v1/data_conversion.py
@@ -62,3 +62,77 @@ def to_tsplib(
     )
 
     return problem
+
+
+def to_tsplib_competition(
+    instance: CVRPInstance, params: Optional[TSPLIBConversionParams] = None
+) -> tsplib95.models.StandardProblem:
+    """Converts a LoggiBUD instance into a TSPLIB95 competition format
+
+    Notes
+    -----
+    Here are specific details for the competition:
+        - The distance matrix must be symmetric: in this case, replace the
+        (i, j)-th element with the average of the (j, i)-th and itself;
+        - The coordinates must be added in the `NODE_COORD_SECTION`;
+        - The distance matrix is explicitly written with a `LOWER_DIAG_ROW`
+        format.
+    """
+
+    params = params or TSPLIBConversionParams()
+
+    num_deliveries = len(instance.deliveries)
+
+    demands_section = {
+        i: delivery.size
+        for i, delivery in enumerate(instance.deliveries, start=2)
+    }
+
+    # Depot demand is always zero.
+    demands_section[1] = 0
+
+    locations = [instance.origin] + [
+        delivery.point for delivery in instance.deliveries
+    ]
+
+    # Create a symmetric distance matrix by replace the (i, j) element with the
+    # average of (i, j) and (j, i)
+    distance_matrix = calculate_distance_matrix_m(
+        locations, config=params.osrm_config
+    )
+    symmetric_distance_matrix = (
+        np.triu(distance_matrix) + np.tril(distance_matrix).T
+    ) / 2  # upper triangular portion
+    symmetric_distance_matrix += np.triu(
+        symmetric_distance_matrix, k=-1
+    ).T  # lower triangular portion
+    scaled_matrix = symmetric_distance_matrix * params.distance_scaling_factor
+
+    # Not well documented, but the API seems to expect a dict mapping location
+    # index with its coordinates
+    node_coords = {
+        i: (location.lng, location.lat)
+        for i, location in enumerate(locations, start=1)
+    }
+
+    # For each row of the distance matrix, write only the elements up to the
+    # main diagonal
+    scaled_matrix_triu = np.tril(scaled_matrix.astype(np.int32)).tolist()
+    edge_weights = [
+        row[:i] for i, row in enumerate(scaled_matrix_triu, start=1)
+    ]
+
+    problem = tsplib95.models.StandardProblem(
+        name=instance.name,
+        type="CVRP",
+        dimension=num_deliveries + 1,
+        node_coords=node_coords,
+        edge_weight_type="EXPLICIT",
+        edge_weight_format="LOWER_DIAG_ROW",
+        edge_weights=edge_weights,
+        demands=demands_section,
+        capacity=instance.vehicle_capacity,
+        depots=[1],
+    )
+
+    return problem
--- tests/v1/test_data_conversion.py
+++ tests/v1/test_data_conversion.py
@@ -25,3 +25,15 @@ def test_can_create_proper_tsplib_from_instance(
     assert toy_cvrp_instance.name in tspfile
     assert "ACVRP" in tspfile
     assert str(toy_cvrp_instance.vehicle_capacity) in tspfile
+
+
+def test_can_create_proper_tsplib_competition_from_instance(
+    toy_cvrp_instance, mocked_osrm_distance_matrix
+):
+    tsplib_instance = data_conversion.to_tsplib_competition(toy_cvrp_instance)
+    tspfile = str(tsplib_instance)
+
+    assert toy_cvrp_instance.name in tspfile
+    assert "CVRP" in tspfile
+    assert "NODE_COORD_SECTION" in tspfile
+    assert str(toy_cvrp_instance.vehicle_capacity) in tspfile
